eded1358327f36632d82e6ce9cad56a556708771
andrelopesmds <andrelopesmds@gmail.com>
2021-06-28 12:12:30 -0300

    Separate script.

--- setup-repo.js
+++ setup-repo.js
@@ -1,10 +1,10 @@
 const { Octokit } = require("@octokit/core");
-const sodium = require('tweetsodium');
 
 const { PERSONAL_TOKEN, NEW_REPOSITORY, DUMMY_SECRET_PROD } = process.env;
 
 const octokit = new Octokit({ auth: PERSONAL_TOKEN });
 
+// Better to have this defined in yml
 const OWNER = 'andrelopesmds';
 const TEMPLATE_REPO = 'template-repository-poc';
 
@@ -28,14 +28,6 @@ const createRepositoryFromTemplate = async (templateOwner, templateRepo, newRepo
   return response.data.id;
 }
 
-const createEnvironment = async (owner, repo, environmentName) => {
-  await octokit.request("PUT /repos/{owner}/{repo}/environments/{environment_name}", {
-    owner,
-    repo,
-    environment_name: environmentName,
-  })
-}
-
 const protectBranch = async (owner, repo, branch) => {
   await octokit.request("PUT /repos/{owner}/{repo}/branches/{branch}/protection", {
     owner,
@@ -53,39 +45,10 @@ const protectBranch = async (owner, repo, branch) => {
   });
 }
 
-const addSecrets = async (repoId, environment, secretName, secretValue) => {
-  // Get an environment public key
-  const response = await octokit.request("GET /repositories/{repository_id}/environments/{environment_name}/secrets/public-key", {
-    repository_id: repoId,
-    environment_name: environment
-  });
-  const { key_id, key } = response.data;
-
-  // Encrypt secret
-  const messageBytes = Buffer.from(secretValue);
-  const keyBytes = Buffer.from(key, 'base64');
-  const encryptedBytes = sodium.seal(messageBytes, keyBytes);
-  const encryptedSecretValue = Buffer.from(encryptedBytes).toString('base64');
-
-  await octokit.request("PUT /repositories/{repository_id}/environments/{environment_name}/secrets/{secret_name}", {
-    repository_id: repoId,
-    environment_name: environment,
-    secret_name: secretName,
-    encrypted_value: encryptedSecretValue,
-    key_id
-  });
-}
-
 const run = async () => {
   try {
     // create repo
-    const repoId = await createRepositoryFromTemplate(OWNER, TEMPLATE_REPO, NEW_REPOSITORY);
-
-    // add secrets (create env + add secrets)
-    await createEnvironment(OWNER, NEW_REPOSITORY, 'prod');
-    await createEnvironment(OWNER, NEW_REPOSITORY, 'dev');
-
-    await addSecrets(repoId, 'prod', 'DUMMY_SECRET_PROD', DUMMY_SECRET_PROD);
+    await createRepositoryFromTemplate(OWNER, TEMPLATE_REPO, NEW_REPOSITORY);   
 
     // add groups (?)
 
